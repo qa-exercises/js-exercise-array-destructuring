5b8e134e4916af61de77011726156df42c657ae2
qa-exercises <118732445+qa-exercises@users.noreply.github.com>
2022-12-22 01:12:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-exercise-array-destructuring
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-exercise-array-destructuring)
---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+
+💪 Exercise: Array destructuring
+
+*/
+
+const emojis = ['🙂', '🙁', '😲']
+
+/*
+
+🎯 Your task:
+- Use array destructuring to rewrite the code below so that the three lines become one single line.
+- The three emojis should be extracted into three separate variables.
+
+*/
+
+const happy = emojis[0]
+const sad = emojis[1]
+const shocked = emojis[2]
+
+// -----------------------------
+
+console.log(happy, sad, shocked)
